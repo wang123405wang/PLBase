784a22e2e95859770a777f8d0411ef5bfd88d3b5
wang123405wang <17767110597@163.com>
2019-11-22 15:43:00 +0800

    1.0,4 头文件重新引入

--- PLBase/Classes/Core/PLBaseCoreDefine.h
+++ PLBase/Classes/Core/PLBaseCoreDefine.h
@@ -8,17 +8,21 @@
 #ifndef PLBaseCoreDefine_h
 #define PLBaseCoreDefine_h
 
-#import "UINavigationController+NavigationBar.h"
-#import "CALayer+XibConfiguration.h"
-#import "UIView+HUD.h"
-#import "BaseXIBView.h"
-#import "UIScrollView+Refresh.h"
-#import "PLBaseModel.h"
-#import "NSObject+Ext.h"
-#import "NSObject+MJExtension.h"
-#import "NSString+PL.h"
-#import "NSObject+AFNetworking.h"
-#import "NotificationConst.h"
-#import "PLNetManager.h"
+#import <UINavigationController+NavigationBar.h>
+#import <CALayer+XibConfiguration.h>
+#import <UIView+HUD.h>
+#import <BaseXIBView.h>
+#import <UIScrollView+Refresh.h>
+#import <PLBaseModel.h>
+#import <NSObject+Ext.h>
+#import <NSObject+MJExtension.h>
+#import <NSString+PL.h>
+#import <NSObject+AFNetworking.h>
+#import <NotificationConst.h>
+#import <PLNetManager.h>
+#import <PLCacheHelper.h>
+#import <PLDataHelper.h>
+#import <PLDateHelper.h>
+#import <PLStyleHelper.h>
 
 #endif /* PLBaseCoreDefine_h */
